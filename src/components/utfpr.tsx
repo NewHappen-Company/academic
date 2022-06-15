ea9e659ab08624cfe80ebffd6969da579b26facb
yuriBaza23 <yuribaza2321@gmail.com>
2022-06-15 10:02:51 -0300

    style: change margin bottom

--- src/components/utfpr.tsx
+++ src/components/utfpr.tsx
@@ -1,10 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react';
 
-function UTFPRLogo() {
+interface IUTFPRLogoProps {
+  withMb?: boolean;
+}
 
+function UTFPRLogo({ withMb = true }: IUTFPRLogoProps) {
   return(
-    <div data-testid='utfpr-logo' className='w-40 rounded h-auto bg-[#F6F6F6] p-6 mb-6 flex items-center justify-center'>
+    <div data-testid='utfpr-logo' className={`w-40 rounded h-auto bg-[#F6F6F6] p-6 ${withMb && 'mb-6'} flex items-center justify-center`}>
       <img src='/utfpr.svg' alt='UTFPR'/>
     </div>
   );
